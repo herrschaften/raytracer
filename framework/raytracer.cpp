d87344e11937e98295973879b16a2a73c016c409
PhilJungschlaeger <PhilJungschlaeger>
2016-08-05 13:26:39 +0200

    :rocket: Raytracer ist organisiert :heart: Errorhandling fehlt..

--- framework/raytracer.cpp
+++ framework/raytracer.cpp
@@ -3,9 +3,18 @@
 #include "raytracer.hpp"
 #include "sdfloader.hpp"
 #include "renderer.hpp"
+//
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <dirent.h>
+
+//
 
 #include <thread>
 #include <fensterchen.hpp>
+
+
 //
 //KONSTRUTOREN----------------------------------------------------------------------
   
@@ -27,70 +36,74 @@
   //Destruktor
 
 //FUNKTIONEN----------------------------------------------------------------------
+ 
   /*FKT 1
   ######################################	
   Aufruf zum rendern. Fkt organisiert in 
   Form einer Schleife alle zu rendernden
   Dateien und übergibt die Scenen dazu an
   den Renderer weiter!*/
+
+  bool has_suffix(const std::string& s, const std::string& suffix) //ausgelagerte Funktion in Lambda?
+  {
+    return (s.size() >= suffix.size()) && equal(suffix.rbegin(), suffix.rend(), s.rbegin());    
+  }
+
   void Raytracer::render()
 	{
     /*
     ######################################
-    Idee.: per while schleife durch Ordner 
-    und die jeweilige.txt datei an den 
-    Renderer schicken...
-    mit boost oder doch lieber Vector oder 
-!   andere Lösung... später!
+    Zum durchsuchen der Ordner siehe Quelle:
+    http://stackoverflow.com/questions/19189014
+    /how-do-i-find-files-with-a-specific-extension
+    -in-a-directory-that-is-provided-by
     ######################################*/
+    
+    DIR *dir = opendir(m_in.c_str());         //In Input-path
+    dirent *entry;                            
+    unsigned int count=1;
 
-    std::string file="file.txt"; //path->file[1]
-    bool done = false;
-    while (!done)  //durch n files
+    while((entry = readdir(dir))!=nullptr)    //durch alle files
     {
-      if (true) //eine ".txt"
-      {
-        Scene scene = SDFLoader::load(file); 
-        std::string outputfile="out.ppm";
-        Renderer rendi(scene, m_width, m_height, outputfile); 
 
-        std::thread thr([&rendi]() { rendi.render(); });
-        
-        ///*
-        Window win(glm::ivec2(m_width,m_height));
+       if(has_suffix(entry->d_name, ".txt"))  // ".txt"?
+        { //Ablauf für eine SDF-Datei:
+          std::cout << "<Datei: " << count << std::endl; 
 
-        while (!win.shouldClose()) {
-          if (win.isKeyPressed(GLFW_KEY_ESCAPE)) {
-              win.stop();
+          std::cout << "-SDFLoader:" << entry->d_name << std::endl;
+          Scene scene = SDFLoader::load(entry->d_name); 
+          
+          std::string outputfile=std::string(m_out)+"/out"+entry->d_name;
+          outputfile =outputfile.substr(0, outputfile.size()-3);//delete txt
+          outputfile+="ppm";
+
+          std::cout << "-outputfile: " << outputfile<< std::endl;
+
+          Renderer rendi(scene, m_width, m_height, outputfile); 
+          std::thread thr([&rendi]() { rendi.render(); });
+        //folgendes nachher löschen, nur zur Ansicht schön..----------------
+          Window win(glm::ivec2(m_width,m_height));
+
+          while (!win.shouldClose()) 
+          {
+            if (win.isKeyPressed(GLFW_KEY_ESCAPE)) 
+            {
+                win.stop();
+            }
+
+            glDrawPixels( m_width, m_height, GL_RGB, GL_FLOAT
+            , rendi.colorbuffer().data());
+
+            win.update();
           }
-
-          glDrawPixels( m_width, m_height, GL_RGB, GL_FLOAT
-          , rendi.colorbuffer().data());
-
-          win.update();
+          thr.join();
+        //-------------------------------------------------------------------
+          count++;
         }
-
-      thr.join();
-      //*/
-
-      }
-      done = true;
     }
 
-    /*
-		if(!fs::exists(m_in) || !fs::is_directory(m_in)) return;
+    closedir(dir);
 
-	    fs::recursive_directory_iterator it(m_in);
-	    fs::recursive_directory_iterator endit;
-    */
-    /*
-	    while(it != endit)
-	    {
-	        if(fs::is_regular_file(*it) && it->path().extension() == ".txt") {
-				std::cout<<it->path().filename();
-			}			
-	        ++it;
 
-	    }
-    */
-	}
+  }
+  
