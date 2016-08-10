bd137e23789e83d41dfea99439edf1ac8c9fc74d
PhilJungschlaeger <PhilJungschlaeger>
2016-08-10 20:57:57 +0200

    :metal: pimped up

--- framework/sphere.cpp
+++ framework/sphere.cpp
@@ -1,11 +1,11 @@
-// sphere.cpp GREAT
+/*sphere.cpp 
+Feel free to sphere around.
+*/
 
 #include "sphere.hpp"
 
  //KONSTRUTOREN----------------------------------------------------------------------
-  
   //Default 
-  //name:Sphere, Color: 0,0,0, center: 0, radius: 1
   Sphere::Sphere() :
   Shape {"Sphere", {}},
   m_center {0.0f, 0.0f, 0.0f}, 
@@ -40,44 +40,35 @@
   }
 
 //GETTER----------------------------------------------------------------------
-
   glm::vec3 const& Sphere::center() const 
   {
     return m_center;
   }
-
   float Sphere::radius() const 
   {
     return m_radius;
   }
 
 //Setter----------------------------------------------------------------------
-
   void Sphere::center(glm::vec3 const& ctr) 
   {
     m_center = ctr;
   }
-
-    // set radius 
   void Sphere::radius(float const& rad) 
   {
     m_radius = rad;
   }
 
 //FUNKTIONEN----------------------------------------------------------------------
-
   float Sphere::area() const 
   {
     return (4.0f*M_PI*pow(m_radius,2.0f));
   }
-
-  
   float Sphere::volume() const 
   {
     return (4.0f*M_PI*pow(m_radius,3.0f)/3.0f);//Betrag unnötig?
   }
-
-   //print
+  //print
   std::ostream& Sphere::print(std::ostream& os) const 
   {
     Shape::print(os); //Name, Color
@@ -91,26 +82,21 @@
 
     return os;
   }
-
-Hit Sphere::intersect(Ray const& ray) const
-  {
-    Hit spherehit;
-
-    spherehit.m_hit = glm::intersectRaySphere(ray.m_origin, ray.m_direction,
-      m_center, m_radius, spherehit.m_point, spherehit.m_normal); //Indirekte param.
-
-    if (spherehit.m_hit)
+  Hit Sphere::intersect(Ray const& ray) const
     {
-      spherehit.m_distance = glm::distance(ray.m_origin, spherehit.m_point);
-      spherehit.m_shape = std::make_shared<Sphere> (*this);
+      Hit spherehit;
+      spherehit.m_hit = glm::intersectRaySphere
+        (
+          ray.m_origin, ray.m_direction,
+          m_center, m_radius, 
+          spherehit.m_point, spherehit.m_normal //Indirekte param.
+        ); 
+
+      if (spherehit.m_hit) //=true ->extra Info
+      {
+        spherehit.m_distance = glm::distance(ray.m_origin, spherehit.m_point);
+        spherehit.m_shape = std::make_shared<Sphere> (*this);
+      }
+        
+      return spherehit;
     }
-      
-    return spherehit;
-  }
-
-
-/*
-  bool Sphere::intersect ( Ray const & r , float & distance ) const { //ray.direction normalisiert bitte!
-    return glm::intersectRaySphere(r.origin_, r.direction_, m_center, m_radius , distance);
-  }
-  */
--- framework/sphere.hpp
+++ framework/sphere.hpp
@@ -1,4 +1,6 @@
-// shape.hpp GREAT
+/*sphere.hpp 
+Feel free to sphere around.
+*/
 
 #ifndef BUW_SPHERE_HPP
 #define BUW_SPHERE_HPP
@@ -8,24 +10,26 @@
 
 class Sphere : public Shape {
 public: 
+  //KONSTRUTOREN----------------------------------------------------------------
   Sphere();
   Sphere(glm::vec3 const& ctr, float rad);
   Sphere(std::string const& name, Material const& mat, glm::vec3 const& ctr, float rad); 
   Sphere(std::string const& name, Material* const& mat, glm::vec3 const& ctr, float rad);//Scne Mater
-
   ~Sphere() override; // Aufgabe 5.8
 
+  //FUNKTIONEN------------------------------------------------------------------
   glm::vec3 const& center() const;
   float radius() const;
+
   void center(glm::vec3 const& ctr);
   void radius(float const& rad);
 
   float area() const override;
   float volume() const override;
+  
   std::ostream& print(std::ostream& os) const override;
-  Hit intersect(Ray const& ray) const override; //Zu Hit geändert: infos auch über Schnittpunkt, statt Bool.
-  //bool intersect ( Ray const & r , float & distance ) const override;
-
+  Hit intersect(Ray const& ray) const override;
+  
 private:
   glm::vec3 m_center;
   float m_radius;
