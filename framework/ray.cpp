14d7ed67333b6266a1e279c909b80189c2a735d5
PhilJungschlaeger <PhilJungschlaeger>
2016-09-07 11:04:42 +0200

    Camera updated with matrix

--- framework/ray.cpp
+++ framework/ray.cpp
@@ -5,5 +5,5 @@ Ray transformRay(glm::mat4 const& mat, Ray const& ray)
 	glm::vec4 a{ray.m_origin,1.0f};
 	glm::vec4 b{ray.m_direction,0.0f};
 
-	return {mat*a,mat*b};
+	return {glm::vec3(mat*a),glm::vec3(mat*b)};
 }
